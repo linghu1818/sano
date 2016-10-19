3b222689820e69de09b6e6cc287b6c5b3dbb12d5
linghu <zhaoyanchang@163.com>
2016-10-19 15:53:57 +0800

    first commit of the project

---
+++ app/src/main/java/com/linghu/sano/TwoActivity.java
@@ -0,0 +1,17 @@
+package com.linghu.sano;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by linghu on 2016/10/19.
+ */
+public class TwoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+}
---
+++ app/src/main/java/com/linghu/sano/dinject/ViewFinder.java
@@ -0,0 +1,12 @@
+package com.linghu.sano.dinject;
+
+import android.view.View;
+
+/**
+ * Created by linghu on 15/5/21.
+ * View的finder,包括Activity、ViewGroup等具有findViewById的类
+ */
+public interface ViewFinder {
+
+    View findViewById(int res);
+}
